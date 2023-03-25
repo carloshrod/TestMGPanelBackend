008d17dc60e9b2d240a155db49361fbeb758b9d6
chrod <chrod.dev@gmail.com>
2023-03-25 12:17:51 -0500

    Change title on DataTableToolbar

--- mg-admin-view/src/components/tables/DataTableToolbar.jsx
+++ mg-admin-view/src/components/tables/DataTableToolbar.jsx
@@ -24,7 +24,7 @@ const DataTableToolbar = () => {
 				id='tableTitle'
 				component='div'
 			>
-				Filter
+				Usuarios registrados
 			</Typography>
 			<Tooltip title='Agregar usuario' arrow>
 				<Button
